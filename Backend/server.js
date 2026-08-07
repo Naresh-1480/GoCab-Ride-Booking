cfb642eb4df711c625eb437d40716b0e2cad33ce
Naresh-1480 <choudharynaresh2005@gmail.com>
2026-08-07 13:28:00 +0530

    User Model Created

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
 const http = require("http");
-const app = require("./app");
+const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
+const connectDB = require("./src/db/db");
 
-const server = http.createServer(app);
+connectDB()
+  .then(() => {
+    const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
-});
+    server.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require(mongoose);
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "First Name must be at least 3 characters long"],
+    },
+    lastname: {
+      type: String,
+      minlength: [3, "Last Name must be at least 3 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+      "Please Enter a Valid Email",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  socketID: {
+    type: String,
+  },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET);
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
